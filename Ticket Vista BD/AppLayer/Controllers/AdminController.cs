b17ffbdfe98f5d36f6b16478d6c9faf4b2574a1d
akashasmaul <akashasmaulhossain@gmail.com>
2023-12-29 21:06:50 +0600

    1st merge

---
+++ Ticket Vista BD/AppLayer/Controllers/AdminController.cs
@@ -0,0 +1,151 @@
+﻿using AppLayer.Auth;
+using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppLayer.Controllers
+{
+    public class AdminController : ApiController
+    {
+        [AdminLogged]
+        [HttpPost]
+        [Route("api/Registration/Admin")]
+        public HttpResponseMessage Create(AdminDTO obj)
+        {
+            try
+            {
+                var data = AdminService.Create(obj);
+                if (data)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Registered Successfully", Data = obj });
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = "Not Registered", Data = obj });
+                }
+            }
+            catch (DbUpdateException dbEx)
+            {
+                Exception innerException = dbEx.InnerException;
+                while (innerException.InnerException != null)
+                    innerException = innerException.InnerException;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = innerException.Message, Data = obj });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = ex.Message, Data = obj });
+            }
+
+        }
+
+
+
+        [AdminLogged]
+        [HttpGet]
+        [Route("api/admin/getAdmins")]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                var data = AdminService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = ex.Message, });
+            }
+
+        }
+
+
+        [AdminLogged]
+        [HttpGet]
+        [Route("api/admin/getAdmin/{id}")]
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                var data = AdminService.Get(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = ex.Message, });
+            }
+
+        }
+
+
+
+        [AdminLogged]
+        [HttpGet]
+        [Route("api/admin/DeleteAdmin/{id}")]
+        public HttpResponseMessage DeleteAdmin(int id)
+        {
+            try
+            {
+
+                var data = AdminService.Delete(id);
+                return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Deleted Succesfully" });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = ex.Message, });
+            }
+
+        }
+
+        [AdminLogged]
+        [HttpPost]
+        [Route("api/admin/UpdateAdmin")]
+        public HttpResponseMessage UpdateAdmin(AdminUpdateDTO obj)
+        {
+            try
+            {
+                var data = AdminService.Update(obj);
+                return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Updated Succesfully", Data = obj });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = ex.Message, Data = obj });
+            }
+
+        }
+
+
+        [AdminLogged]
+        [HttpGet]
+        [Route("api/admin/ViewProfile/{id}/{Token}")]
+        public HttpResponseMessage ViewProfile(int id,string Token)
+        {
+            try
+            {
+                if( AuthService.ValidUser(Token,id) )
+                {
+
+                    var data = AdminService.UnprotectedGet(id);
+                    return Request.CreateResponse(HttpStatusCode.OK, data);
+                }
+                else
+                {
+                     return Request.CreateResponse(HttpStatusCode.NotFound, new { Msg = "Wrong Token or Id" });
+                }
+
+            }
+            catch(Exception ex) 
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = ex.Message, });
+            }
+
+        }
+
+    
+
+    }
+}
---
+++ Ticket Vista BD/BLL/Services/AdminService.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.Models;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+     public class AdminService
+    {
+        public static List<AdminViewDTO> Get()
+        {
+            var data = DataAccessFactory.AdminData().Read();
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Admin, AdminViewDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<List<AdminViewDTO>>(data);
+            return mapped;
+        }
+
+        public static AdminViewDTO Get(int id)
+        {
+            var data = DataAccessFactory.AdminData().Read(id);
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Admin, AdminViewDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<AdminViewDTO>(data);
+            return mapped;
+        }
+
+        public static AdminDTO UnprotectedGet(int id)
+        {
+            var data = DataAccessFactory.AdminData().Read(id);
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Admin, AdminDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<AdminDTO>(data);
+            return mapped;
+        }
+
+        public static bool Create(AdminDTO obj)
+        {
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<AdminDTO, Admin>();
+            });
+            var mapper = new Mapper(cfg);
+            var admin = mapper.Map<Admin>(obj);
+
+            var result = DataAccessFactory.AdminData().Create(admin);
+
+            return result;
+        }
+
+        public static bool Delete(int id) 
+        {
+            return DataAccessFactory.AdminData().Delete(id);
+
+        }
+
+        public static bool Update( AdminUpdateDTO obj)
+        {
+
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<AdminUpdateDTO, Admin>();
+            });
+            var mapper = new Mapper(cfg);
+            var admin = mapper.Map<Admin>(obj);
+
+            var result = DataAccessFactory.AdminData().Update(admin);
+
+            return result;
+        }
+
+
+    }
+}
