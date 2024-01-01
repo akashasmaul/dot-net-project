2fe10922ea694fcb157fb7d5c26afd721df30bdd
akashasmaul <akashasmaulhossain@gmail.com>
2024-01-02 01:34:13 +0600

    Marge 3

---
+++ Ticket Vista BD/AppLayer/Controllers/TicketController.cs
@@ -0,0 +1,51 @@
+﻿using AppLayer.Auth;
+using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppLayer.Controllers
+{
+    public class TicketController : ApiController
+    {
+
+        [BuyerLogged]
+        [HttpPost]
+        [Route("api/Buyer/CreateTicket")]
+        public HttpResponseMessage Create(TicketDTO obj)
+        {
+            try
+            {
+
+                var data = TicketService.Create(obj);
+                if (data)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Ticket Created Successfully", Data = obj });
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = "Not Created", Data = obj });
+                }
+            }
+            catch (DbUpdateException dbEx)
+            {
+                Exception innerException = dbEx.InnerException;
+                while (innerException.InnerException != null)
+                    innerException = innerException.InnerException;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = innerException.Message, Data = obj });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = ex.Message, Data = obj });
+            }
+
+        }
+
+
+    }
+}
